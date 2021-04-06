12e64560243b819f0dd5b2d90aba243ed2df1526
刘赛登 <liusd@tuya.com>
2021-04-06 10:56:02 +0800

    临时提交

---
+++ app/src/main/java/com/hiy/camerap/SplashAc.java
@@ -0,0 +1,32 @@
+package com.hiy.camerap;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashAc extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        View view = findViewById(android.R.id.content);
+        view.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashAc.this, MainActivity.class);
+                startActivity(intent);
+            }
+        }, 1500);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+
+    }
+}
