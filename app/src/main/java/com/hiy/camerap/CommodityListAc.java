6ce372243e323f662eca2b97e0fe082b6361f293
刘赛登 <pepperer@qq.com>
2021-04-08 14:46:46 +0800

    bomb api

--- app/src/main/java/com/hiy/camerap/CommodityListAc.java
+++ app/src/main/java/com/hiy/camerap/CommodityListAc.java
@@ -1,7 +1,6 @@
 package com.hiy.camerap;
 
 import android.os.Bundle;
-import android.util.Log;
 
 import androidx.annotation.Nullable;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -9,19 +8,11 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.hiy.camerap.adapter.CommodityBinder;
 import com.hiy.camerap.bean.Commodity;
-import com.hiy.camerap.interceptor.BmobInterceptor;
 
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 import me.drakeet.multitype.MultiTypeAdapter;
-import okhttp3.Call;
-import okhttp3.Callback;
-import okhttp3.HttpUrl;
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.Response;
 
 /**
  * @author zhishui <a href="mailto:liusd@tuya.com">Contact me.</a>
@@ -53,32 +44,7 @@ public class CommodityListAc extends BaseAc {
     }
 
     private void loadData() {
-        OkHttpClient client = new OkHttpClient.Builder()
-                .addInterceptor(new BmobInterceptor())
-                .build();
-
-        HttpUrl httpUrl = new HttpUrl.Builder()
-                .scheme("https")
-                .host("api2.bmob.cn")
-                .addPathSegments("1/classes/commodity")
-                .build();
-
-        Log.d(tag, httpUrl.toString());
-
-        Request request = new Request.Builder().url(httpUrl.toString()).build();
-
-        client.newCall(request).enqueue(new Callback() {
-            @Override
-            public void onFailure(Call call, IOException e) {
-                Log.d(tag, "onFailure-" + e.getMessage());
-            }
-
-            @Override
-            public void onResponse(Call call, Response response) throws IOException {
-                String resString = response.body().string();
-                Log.d(tag, "response-" + resString);
-            }
-        });
+        BmobAPi.getApi(BmobAPi.S_API_commodity_LIST);
     }
 
 }
